0f324a319c96f7d5067cc5ab01abd6d8f2c089be
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-06-13 13:19:23 +0530

    popular set

---
+++ Frontend/src/assets/popular.js
@@ -0,0 +1,28 @@
+import p5_img from './product_5.png'
+import p19_img from './proudct_19.png'
+import p32_img from './product_32.png'
+
+const popular=[
+    {
+        id:1,
+        name:'popular 1',
+        image:p5_img,
+        oldp:25.00,
+        newp:45,
+    },
+    {
+        id:2,
+        name:'popular 2',
+        image:p19_img,
+        oldp:25.00,
+        newp:45,
+    },
+    {
+        id:3,
+        name:'popular 3',
+        image:p32_img,
+        oldp:25.00,
+        newp:45,
+    }
+]
+export default popular
--- Frontend/src/pages/popular.jsx
+++ Frontend/src/pages/popular.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 
 const Popular = () => {
   return (
-    <div className="text-center">
+    <>
+     <div className="text-center">
       <div className="inline-block">
         <h3 className='text-2xl font-bold'>Popular Items</h3>
         <hr className='border-2 border-black mt-2' /> {/* Line fits text width */}
       </div>
     </div>
+    <div>
+          
+
+    </div>
+    </>
+   
   );
 };
 
