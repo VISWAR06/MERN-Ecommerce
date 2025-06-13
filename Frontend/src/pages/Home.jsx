82f0961ba6aa271884ece5c905cb34f38629d066
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-06-13 11:49:39 +0530

    frnt page banner

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,15 +2,25 @@ import React from 'react';
 import imge from '../assets/hero_image.png';
 import hi from '../assets/hand_icon.png';
 import { Link } from 'react-router-dom';
+
 const Home = () => {
   return (
-    <div className="mt-14 bg-red-400 h-1/2 w-full">
-      <div className='pt-[240px] pl-[450px]'><h1>Welcome</h1>
-      <h2 className='flex '> new arrivals <img src={hi} alt ='hi' className='h-4 mt-1'/></h2>
-       <button><Link to='/new'>New Collection's</Link></button> </div>
-      
-      <img src={imge} alt="img" className="h-1/2 mx-[950px] pb-[350px]" />
-    
+    <div className="mt-14  h-[800px] text-6xl font-extrabold flex w-full">
+      {/* Left Side: Image */}
+      <div className="w-1/2 flex justify-center items-center">
+        <img src={imge} alt="hero" className="w-[90%] h-[800px] object-contain" />
+      </div>
+
+      {/* Right Side: Text */}
+      <div className="w-1/2 flex flex-col justify-center items-center gap-6">
+        <h1>Welcome</h1>
+        <h2 className="flex items-center gap-4">
+          new arrivals <img src={hi} alt="hi" className="h-20 mb-2" />
+        </h2>
+        <button className="bg-purple-600 rounded-2xl text-2xl p-3">
+          <Link to="/new">New Collection's</Link>
+        </button>
+      </div>
     </div>
   );
 };
