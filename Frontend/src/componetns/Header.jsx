c7294e3648e2b6ade494a19c16d1620935eb4897
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-06-13 09:24:11 +0530

    header finish

--- Frontend/src/componetns/Header.jsx
+++ Frontend/src/componetns/Header.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 const Header = () => {
   return (
-    <div >
+    <div className='flex justify-between items-center w-full h-12 shadow-lg'>
       <div>LOGO</div>
-      <div >
+      {/* <div className=''>
         <Link to='/'>Shop</Link>
         <Link to='/men'>Men</Link>
         <Link to='/women'>Women</Link>
         <Link to='/kids'>Kids</Link>
-      </div>
-      <div className='felx gap-6'>
-        <button>login</button>
+      </div> */}
+      <div className='flex gap-4'>
+        <button className='bg-red-400 rounded-2xl cursor-pointer'>login</button>
         <button>cart</button>
         <button>profile</button>
       </div>
