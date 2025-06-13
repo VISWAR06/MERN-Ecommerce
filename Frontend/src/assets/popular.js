c6c86e6283d1bcba43d962dedccafbfd7cce5cd1
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-06-13 14:37:19 +0530

    popular images

--- Frontend/src/assets/popular.js
+++ Frontend/src/assets/popular.js
@@ -1,5 +1,5 @@
 import p5_img from './product_5.png'
-import p19_img from './proudct_19.png'
+import p19_img from './product_19.png'
 import p32_img from './product_32.png'
 
 const popular=[
@@ -8,21 +8,21 @@ const popular=[
         name:'popular 1',
         image:p5_img,
         oldp:25.00,
-        newp:45,
+        newp:45
     },
     {
         id:2,
         name:'popular 2',
         image:p19_img,
         oldp:25.00,
-        newp:45,
+        newp:45
     },
     {
         id:3,
         name:'popular 3',
         image:p32_img,
         oldp:25.00,
-        newp:45,
+        newp:45
     }
 ]
 export default popular
--- Frontend/src/pages/popular.jsx
+++ Frontend/src/pages/popular.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
-
+import popularr from '../assets/popular'
 const Popular = () => {
+  
   return (
     <>
      <div className="text-center">
       <div className="inline-block">
         <h3 className='text-2xl font-bold'>Popular Items</h3>
-        <hr className='border-2 border-black mt-2' /> {/* Line fits text width */}
+        <hr className='border-2 border-black mt-2' /> 
       </div>
     </div>
-    <div>
-          
+    <div className='flex justify-center gap-4 h-64 py-4'>
+    {popularr.map(item=>(
+      <div key={item.id } className='text-center'>
+        <img src={item.image} alt={item.name} className='h-full'/>
+        <h4 className=''>{item.name}</h4>
+        </div>
+    ))}
 
     </div>
     </>
