be3664dc978fcf345e9fa11571ea930f70f18c35
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-06-16 18:57:52 +0530

    create usere

--- Backend/controllers/usercontroll.js
+++ Backend/controllers/usercontroll.js
@@ -14,6 +14,16 @@ const register=async(req,res)=>{
     }
     const exist = await usermodel.findOne({email})
     if(exist)return res.status(400).json({message:'user already'})
+    if(password.length<8)return res.status(400).json("provide strng password")
+    const salt =await bcrypt.genSalt(10)
+    const hash=await bcrypt.hash(password,salt)
+
+    const newuser= new usermodel({
+        name,
+        email,
+        password:hash
+    })
+    const save = await newuser.save()
 
 }
 
