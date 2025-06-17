4fe74737591db374b4bb9c44ebfc2f1665cbf392
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-06-17 23:19:09 +0530

    product contrl initalize

---
+++ Backend/controllers/productcontroller.js
@@ -0,0 +1,13 @@
+const addprdt = async (req,res)=>{
+
+}
+const listprdt = async(req,res)=>{
+
+}
+const removeprdt = async(req,res)=>{
+
+}
+const single=async(req,res)=>{
+
+}
+export {addprdt,listprdt,removeprdt,single}
