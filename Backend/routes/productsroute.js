4b986f6c896770ffa04ec62de0717cd4930f812e
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-06-18 17:19:10 +0530

    multer userd

--- Backend/routes/productsroute.js
+++ Backend/routes/productsroute.js
@@ -1,8 +1,9 @@
 import express  from 'express'
 import { addprdt,removeprdt,listprdt,singleprdt } from '../controllers/productcontroller.js'
+import upload from '../middlewares/multer.js'
 const productroute = express.Router()
 
-productroute.post('/add',addprdt)
+productroute.post('/add',upload.fields([{name:'image1',maxCount:1},{name:'image2',maxCount:1},{name:'image3',maxCount:1},{name:'image4',maxCount:1},]),addprdt)
 productroute.post('/remove',removeprdt)
 productroute.get('/list',listprdt)
 productroute.post('/single',singleprdt)
