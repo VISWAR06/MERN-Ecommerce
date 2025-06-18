ac0d07ae7d840bce08dcf6faeb02284736c303d4
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-06-18 17:16:11 +0530

    route comp

--- Backend/routes/productsroute.js
+++ Backend/routes/productsroute.js
@@ -1,10 +1,10 @@
 import express  from 'express'
-import { addprdt,removeprdt,listprdt,singleprdt } from '../controllers/productcontroller'
+import { addprdt,removeprdt,listprdt,singleprdt } from '../controllers/productcontroller.js'
 const productroute = express.Router()
 
 productroute.post('/add',addprdt)
 productroute.post('/remove',removeprdt)
-productroute.post('/list',listprdt)
+productroute.get('/list',listprdt)
 productroute.post('/single',singleprdt)
 
 export default productroute
