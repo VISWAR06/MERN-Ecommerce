0a97aad0cc5b534bca38df3696e19f78efcde081
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-07-06 17:38:59 +0530

    processing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+ 
+import Navbar from "./components/navbar/Navbar"
+import {BrowserRouterDom,Route,Routes}from 'react-router-dom'
+function App() {
+
+
+  return (
+    <BrowserRouterDom>
+      <div>
+    <Navbar/>
+    <Routes>
+      
+    </Routes>
+  </div>
+  </BrowserRouterDom>
+
+  )
+}
+
+export default App
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from '../Assets/Frontend_Assets/all_product'
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+            <img src={logo} alt="" />
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
