3d93666b42bb6da2cd08a4118129626866cf7722
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-07-06 19:23:42 +0530

    processing

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
-
+import instagrm from '../Assets/Frontend_Assets/instagram_icon.png'
+import pintrest from '../Assets/Frontend_Assets/pintester_icon.png'
+import logo from '../Assets/Frontend_Assets/logo.png'
 const Footer = () => {
   return (
-    <div>
+    <div className='bg-green-400'>
+        <div>
+            <img src={logo} alt="shpeee" />
+        </div>
+        <div>
+            <ul> <li>Company</li>
+            <li> Products</li>
+            <li>About</li></ul>
+           
+        </div>
+        <div>
+            <img src={instagrm} alt="" />
+        </div>
+        <div>
+            <img src={pintrest} alt="" />
+        </div>
+        <hr/>
+        <div>
+Copyright @ 2025 - All Rights Reserved
+        </div>
       
     </div>
   )
--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -4,6 +4,7 @@ import Hero from '../components/Hero'
 import Womensitem from '../components/Womensitem'
 import Offers from '../components/Offers'
 import Newcollections from '../components/Newcollections'
+import Footer from '../components/Footer'
 
 const Shop = () => {
   return (
@@ -12,6 +13,7 @@ const Shop = () => {
       <Womensitem/>
       <Offers/>
       <Newcollections/>
+      <Footer/>
     </div>
   )
 }
