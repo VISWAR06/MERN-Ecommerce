d94f56dc6798ea38bbebc6b9f57ec956a70eb1c2
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-07-06 22:05:38 +0530

    processing

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -9,12 +9,12 @@ const Login = () => {
         <input type='email' placeholder='Email'/>
         <input type='text' placeholder='Password'/>
       </div>
-      <div>
-        <p>Forgetten Password</p>
-        <p><Link to ='/register'>Register</Link></p>
+      <div className='gap-2'>
+        <p>Forgetten Password
+        <spam className='ml-2 font-bold text-orange-700'><Link to ='/register'>Register</Link></spam></p>
       </div>
       <div>
-        <button>Sigin</button>
+        <button>SIGNIN</button>
       </div>
     </div>
   )
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 const Register = () => {
   return (
     <div>
         <div>Signup</div>
         <div>
-            <input
+            <input type='text' placeholder='Name'/>
+            <input type='email' placeholder='Email'/>
+            <input type='text' placeholder='Password'/>
+
+        </div>
+        <div>
+          <p> Already have an account? <span><Link to='/login'>Sigin</Link></span></p>
+        </div>
+        <div className='flex gap-2'>
+          <input type='checkbox'/> <p> I agree to the terms of use & Privacy policy</p>
+        </div>
+        <div>
+          <button>SIGNUP</button>
         </div>
      
     </div>
