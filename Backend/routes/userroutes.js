0e2df8bd1cf1641a7b37e3775b9b6c44c2ed8bec
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-07-07 14:45:16 +0530

    backend

--- Backend/routes/userroutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import {login,register,admin} from '../controllers/usercontroll.js'
-const route = express.Router()
-route.post('/login',login)
-route.post('/register',register)
-route.post('/admin',admin)
-
-export default route
