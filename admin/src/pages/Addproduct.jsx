8b973dc613f268d25c846a3ccd269dee6fe75ec0
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-07-09 11:59:40 +0530

    admiin

--- admin/src/pages/Addproduct.jsx
+++ admin/src/pages/Addproduct.jsx
@@ -2,18 +2,30 @@ import React, { useState } from 'react'
 import uploadarea from '../assets/Admin_Assets/upload_area.svg'
 const Addproduct = () => {
     const [image,setImage]=useState(false)
+    const[productdetail,setProductDetail]=useState({
+        name:"",
+        new_price:"",
+        category:"men",
+        image:""
+    })
     const imagesetter=(e)=>{
       return   setImage(e.target.files[0])
+    }
+    const handlechange= (e)=>{
+        setProductDetail({...productdetail,[e.target.name]:e.target.value})
+    }
+    const add=async()=>{
+        console.log(productdetail)
     }
   return (
     <div>
       <div>
         <p>Product name</p>
-        <input type="text" name="name" placeholder='Type here' />
+        <input onChange={handlechange} value={productdetail.name} type="text" name="name" placeholder='Type here' />
         <p>price</p>
-        <input type="text" name="new_price" placeholder='Type Price' />
+        <input onChange={handlechange} value={productdetail.price} type="text" name="new_price" placeholder='Type Price' />
        <p>category</p>
-        <select>
+        <select onChange={handlechange} value={productdetail.category}>
             <option value="men">men</option>
             <option value='women'>women</option>
             <option value='kid'>kid</option>
@@ -23,7 +35,7 @@ const Addproduct = () => {
          <input onChange={imagesetter}type='file' name='image' id='file-input' hidden />
         </div>
       </div>
-      <button>Add Product</button>
+      <button onClick={()=>{add()}}>Add Product</button>
     </div>
   )
 }
