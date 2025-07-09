042b63d5f849731ffee05cee857217ca0e2f831e
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-07-09 10:15:38 +0530

    admin

--- admin/src/pages/Addproduct.jsx
+++ admin/src/pages/Addproduct.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import uploadarea from '../assets/Admin_Assets/upload_area.svg'
 const Addproduct = () => {
   return (
     <div>
-      added
+      <div>
+        <input type="text" name="name" placeholder='Type here' />
+        <input type="text" name="new_price" placeholder='Type Price' />
+       
+        <select>
+            <option value="men">men</option>
+            <option value='women'>women</option>
+            <option value='kid'>kid</option>
+        </select>
+        <div>
+            <label htmlFor='file-input'>   <img src={uploadarea} alt="" /></label>
+         <input type='file' name='image' id='file-input' hidden />
+        </div>
+      </div>
+      <button>Add Product</button>
     </div>
   )
 }
