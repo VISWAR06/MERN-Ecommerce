fe0505bcf31718be7792d6a6bce0dce3f2067061
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-07-12 15:24:50 +0530

    user

--- Backend/Routes/Userroutes.js
+++ Backend/Routes/Userroutes.js
@@ -1,7 +1,8 @@
 const express=require('express')
 const route=express.Router()
+const auth=require('../middlewares/Auth')
 const {signup,login}=require('../Controller/Usercontrller')
 
-route.post('/signup',signup)
-route.post('/login',login)
+route.post('/signup',auth,signup)
+route.post('/login',auth,login)
 module.exports=route
