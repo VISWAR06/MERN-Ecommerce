42e40b7cb9a85b0ed077a38f41024fdfc021262c
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-07-18 16:07:29 +0530

    css strt

--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 const Item = (props) => {
   return (
-    <div className="w-82 h-82 p-4 border rounded-lg  ">
+    <div className="w-72 p-4 border rounded-lg  ">
       <Link to={`/product/${props.id}`} onClick={() => window.scrollTo(0, 0)}>
-        <img src={props.image} alt="product" className="w-full h-fit object-cover mb-3" />
+        <img src={props.image} alt="product" className="w-full h-64 object-cover mb-3" />
       </Link>
       <h3 className="text-lg font-semibold truncate">{props.name}</h3>
       <h2 className="text-blue-600 font-bold">${props.new_price}</h2>
