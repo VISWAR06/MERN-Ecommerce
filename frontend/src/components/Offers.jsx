828323a49ba727bc0a21f7f3b9069701c9aab354
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-07-18 16:15:58 +0530

    css strt

--- frontend/src/components/Offers.jsx
+++ frontend/src/components/Offers.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import exclusive from '../Assets/Frontend_Assets/exclusive_image.png'
+import React from 'react';
+import exclusive from '../Assets/Frontend_Assets/exclusive_image.png';
+
 const Offers = () => {
   return (
-    <div className='bg-purple-200'>
-      <div>
-        <h1>Exclusive offers</h1>
-        <h2>Offers for you</h2>
-        <h3>Only on the sellers products</h3>
-      </div>
-      <div>
-        <img src={exclusive} alt="img" />
+    <div className="bg-purple-200 p-6">
+      <div className="flex flex-col md:flex-row items-center justify-between gap-8">
+        
+       
+        <div className="flex flex-col text-end space-y-3 md:w-1/2 ">
+          <h1 className="text-3xl font-bold">Exclusive Offers</h1>
+          <h2 className="text-xl font-semibold">Offers for You</h2>
+          <h3 className="text-md">Only on the seller's products</h3>
+        </div>
+
+       
+        <div className="md:w-1/2 flex justify-start">
+          <img
+            src={exclusive}
+            alt="Exclusive Offer"
+            className="w-[300px] h-auto object-contain"
+          />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Offers
+export default Offers;
