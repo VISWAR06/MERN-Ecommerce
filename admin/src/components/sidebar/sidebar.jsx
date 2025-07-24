851a49f41209a7ee30e9d0c9c43228aaac641576
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-07-24 21:00:25 +0530

    add prdt

--- admin/src/components/sidebar/sidebar.jsx
+++ admin/src/components/sidebar/sidebar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 const sidebar = () => {
   return (
-    <div className=''>
-   <Link to='/addproduct'>Add product</Link>
-   <Link to='/allproduct'>All Product</Link>
+    <div className='flex gap-10'>
+   <Link to='/addproduct' className='border bg-blue-700 text-white rounded-sm'>Add product</Link>
+   <Link to='/allproduct' className='border bg-blue-700 text-white rounded-sm'>All Product</Link>
 
     </div>
   )
