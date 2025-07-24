0be28b627f5236a0b7e333adccb26602429d7577
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-07-24 20:44:09 +0530

    navbar

--- admin/src/components/nabar/Navbar.jsx
+++ admin/src/components/nabar/Navbar.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import logo from '../../assets/admin_assets/nav-logo.svg'
-import profile from '../../assets/Admin_Assets/nav-profile.svg'
+import React from 'react';
+import logo from '../../assets/admin_assets/nav-logo.svg';
+import profile from '../../assets/Admin_Assets/nav-profile.svg';
+
 const Navbar = () => {
   return (
-    <div className='flex'>
-   <div>
-    <img src={logo} alt="logo" />
-   </div>
+    <nav className="w-full bg-white shadow-md px-6 py-4 flex items-center justify-between">
+      {/* Left: Logo */}
+      <div className="flex items-center gap-2">
+        <img src={logo} alt="logo" className="h-10 w-auto" />
+        <span className="text-xl font-semibold text-gray-800">Admin Panel</span>
+      </div>
    <div>
     <img src={profile} alt="profile" />
    </div>
